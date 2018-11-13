5dfb07efe8673af33b4cbf88eb4e81b8ff340747
Wassel Alazhar <wassel.alazhar@gmail.com>
2018-11-13 07:25:43 +0100

    refactor: change CommandBusFactory.build return type to CommandBusMiddleware interface

--- src/main/java/hotel/cuzco/booking/command/CommandBusFactory.java
+++ src/main/java/hotel/cuzco/booking/command/CommandBusFactory.java
@@ -3,6 +3,7 @@ package hotel.cuzco.booking.command;
 import hotel.cuzco.booking.domain.MailSender;
 import hotel.cuzco.booking.domain.ReservationMadeNotifier;
 import hotel.cuzco.booking.domain.RoomRepository;
+import hotel.cuzco.middleware.commands.CommandBusMiddleware;
 import hotel.cuzco.middleware.commands.CommandDispatcher;
 import hotel.cuzco.middleware.commands.EventDispatcherBusMiddleware;
 import hotel.cuzco.middleware.events.EventBus;
@@ -12,8 +13,8 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public class CommandBusFactory {
-    public static EventDispatcherBusMiddleware build(RoomRepository roomRepository,
-                                                     MailSender mailSender) {
+    public static CommandBusMiddleware build(RoomRepository roomRepository,
+                                             MailSender mailSender) {
         var commandHandlers = asList(
                 new MakeReservationCommandHandler(roomRepository),
                 new CancelReservationCommandHandler(roomRepository));
